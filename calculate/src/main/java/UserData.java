6360a860ef8fa8bc04966b166412254580ff9f2a
ve1heor <ve1heor@mail.ru>
2016-02-29 14:10:18 +0300

    abs

--- calculate/src/main/java/UserData.java
+++ calculate/src/main/java/UserData.java
@@ -1,6 +1,4 @@
-
 import java.io.Serializable;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -10,15 +8,38 @@ public class UserData implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private String name;
-	public String getName() {
-		return name;
+	private Double first;
+	private Double second;
+	private Double result;
+
+	public Double getFirst() {
+		return first;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setFirst(Double first) {
+		this.first = first;
 	}
 
-	public String getWelcomeMessage(){
-		return "Hello " + name;
+	public Double getSecond() {
+		return second;
+	}
+	public void setSecond(Double second) {
+		this.second = second;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void addition() {
+		result = first + second;
+	}
+	public void subtraction() {
+		result = first - second;
+	}
+	public void multiply() {
+		result = first * second;
+	}
+	public void divide() {
+		result = first / second;
 	}
 }
